2a00003815e49cec1a0c44bb15dce6cc46afb5e2
Michele Miotti <michele.miotti@mail.polimi.it>
2024-05-20 09:42:04 +0200

    Add additional test cases

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -19,23 +19,35 @@ TEST(Problem2, IsIsogram) {
 
 TEST(Problem3, PossiblyPerfect) {
     EXPECT_TRUE(possibly_perfect({'B', 'A', '_', '_'}, {'B', 'A', 'C', 'C'}));
+    EXPECT_TRUE(possibly_perfect({'_', '_', '_', '_'}, {'B', 'A', 'C', 'C'}));
+    EXPECT_TRUE(possibly_perfect({'D', 'C', 'B', 'A'}, {'B', 'A', 'C', 'C'}));
+    EXPECT_FALSE(possibly_perfect({'A', 'B', '_', '_'}, {'A', 'A', 'C', 'C'}));
+    EXPECT_FALSE(possibly_perfect({'A', 'B', 'C', '_'}, {'A', 'B', 'D', 'C'}));
     EXPECT_TRUE(possibly_perfect({'A', '_', '_', '_'}, {'B', '_', '_', '_'}));
 }
 
 TEST(Problem4, FindOdd) {
     EXPECT_EQ(findOdd({7}), 7);
     EXPECT_EQ(findOdd({0}), 0);
+    EXPECT_EQ(findOdd({0, 1, 1, 1, 1}), 0);
+    EXPECT_EQ(findOdd({2, 2, 0, 0, 0, 1, 1, 1, 1}), 0);
+    EXPECT_EQ(findOdd({2, 2, 0, 0, 0, 0, 1, 1, 1, 1, 1}), 1);
 }
 
 TEST(Problem5, EncodeDecode) {
     EXPECT_EQ(encode("hello"), "h2ll4");
     EXPECT_EQ(decode("h3 th2r2"), "hi there");
+    EXPECT_EQ(encode("hello world"), "h2ll4 w4rld");
+    EXPECT_EQ(decode("h2ll4 w4rld"), "hello world");
 }
 
 TEST(Problem6, Bananas) {
     std::unordered_set<std::string> expected = {"b-anana--", "b-anan--a", "b-ana--na", "b-an--ana", "b-a--nana", "b---anana",
                                                 "-banana--", "-banan--a", "-bana--na", "-ban--ana", "-ba--nana", "-b--anana"};
     EXPECT_EQ(bananas("bbananana"), expected);
+
+    expected = {"-banana", "b-anana"};
+    EXPECT_EQ(bananas("bbanana"), expected);
 }
 
 int main(int argc, char **argv) {
